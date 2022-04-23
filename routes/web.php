3f328319a175bc8e7356c30aa4fadfebca0435fc
Piusshungu <shungupius@gmail.com>
2022-04-23 17:57:25 +0300

    Changed a user profile route to receive id instead of user instance

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware'=>'auth'],function(){
-    Route::get('/profile/{user}',[UserController::class,'profile'])->name('profile');
+    Route::get('/profile/{id}',[UserController::class,'profile'])->name('profile');
     Route::put('/users/{user}/update',[UserController::class,'update'])->name('users.update');
     Route::resource('links',ShortLinkController::class);
     Route::put('/links/{link}/deactivate',[ShortLinkController::class,'deactivate'])->name('links.deactivate');
