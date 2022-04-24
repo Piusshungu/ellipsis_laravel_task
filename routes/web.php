9e67dac81ece0b53dcc2d3bc3f8058381eaa94a4
Piusshungu <shungupius@gmail.com>
2022-04-24 08:19:42 +0300

    Added authentication class in route class

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShortLinkController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
